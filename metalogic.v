ea90a8bbc9f54bbac016fa99ae4ad0de1f1a5786
KiJeong-Lim <gijungdduk@naver.com>
2020-09-07 14:29:48 +0900

    .

--- metalogic.v
+++ metalogic.v
@@ -167,29 +167,157 @@ Module PropositionalLogic.
       infers hs a
     .
 
+    Lemma assume_more_then_also_derivable :
+      forall hs1 : list formula,
+      forall a : formula,
+      infers hs1 a ->
+      forall hs2 : list formula,
+      (forall h : formula, In h hs1 -> In h hs2) ->
+      infers hs2 a.
+    Proof.
+      intros hs1 a.
+      intro.
+      induction H.
+      - intros hs2.
+        intro.
+        apply (Assumption hs2 conclusion).
+        apply (H0 conclusion H).
+      - intros hs2.
+        intro.
+        apply (BottomIntro hs2 a).
+        apply (IHinfers1 hs2 H1).
+        apply (IHinfers2 hs2 H1).
+      - intros hs2.
+        intro.
+        apply (BottomElim hs2 a).
+        apply (IHinfers hs2 H0).
+      - intros hs2.
+        intro.
+        apply (NotIntro hs2 a).
+        apply (IHinfers (a :: hs2)).
+        simpl.
+        intuition.
+      - intros hs2.
+        intro.
+        apply (NotElim hs2 a).
+        apply (IHinfers (Negation a :: hs2)).
+        simpl.
+        intuition.
+      - intros hs2.
+        intro.
+        apply (AndIntro hs2 a b).
+        apply (IHinfers1 hs2 H1).
+        apply (IHinfers2 hs2 H1).
+      - intros hs2.
+        intro.
+        apply (AndElim1 hs2 a b).
+        apply (IHinfers hs2 H0).
+      - intros hs2.
+        intro.
+        apply (AndElim2 hs2 a b).
+        apply (IHinfers hs2 H0).
+      - intros hs2.
+        intro.
+        apply (OrIntro1 hs2 a b).
+        apply (IHinfers hs2 H0).
+      - intros hs2.
+        intro.
+        apply (OrIntro2 hs2 a b).
+        apply (IHinfers hs2 H0).
+      - intros hs2.
+        intro.
+        apply (OrElim hs2 a b c).
+        apply (IHinfers1 hs2 H2).
+        apply (IHinfers2 (a :: hs2)).
+        simpl.
+        intuition.
+        apply (IHinfers3 (b :: hs2)).
+        simpl.
+        intuition.
+      - intros hs2.
+        intro.
+        apply (IfthenIntro hs2 a b).
+        apply (IHinfers (a :: hs2)).
+        simpl.
+        intuition.
+      - intros hs2.
+        intro.
+        apply (IfthenElim hs2 a b).
+        apply (IHinfers1 hs2 H1).
+        apply (IHinfers2 hs2 H1).
+      - intros hs2.
+        intro.
+        apply (IffIntro hs2 a b).
+        apply (IHinfers1 (a :: hs2)).
+        simpl.
+        intuition.
+        apply (IHinfers2 (b :: hs2)).
+        simpl.
+        intuition.
+      - intros hs2.
+        intro.
+        apply (IffElim1 hs2 a b).
+        apply (IHinfers1 hs2 H1).
+        apply (IHinfers2 hs2 H1).
+      - intros hs2.
+        intro.
+        apply (IffElim2 hs2 a b).
+        apply (IHinfers1 hs2 H1).
+        apply (IHinfers2 hs2 H1).
+    Qed.
+
+    Theorem implication_is_equal_to_assumption :
+      forall hs : list formula,
+      forall a b : formula,
+      infers hs (Implication a b) <-> infers (a :: hs) b.
+    Proof.
+      assert (
+        forall hs : list formula,
+        forall a b : formula,
+        infers hs (Implication a b) -> infers (a :: hs) b
+      ).
+        intros hs a b.
+        intro.
+        cut (infers (a :: hs) (Implication a b)).
+          intro.
+          apply (IfthenElim (a :: hs) a b H0).
+          apply (Assumption (a :: hs) a).
+          simpl.
+          tauto.
+          apply (assume_more_then_also_derivable hs (Implication a b) H (a :: hs)).
+          simpl.
+          intuition.
+      intros hs a b.
+      constructor.
+        apply (H hs a b).
+        apply (IfthenIntro hs a b).
+    Qed.
+
   End InferenceRules.
-  
+
   Section Soundness.
 
-    Theorem soundness_of_propositional_logic :
-      forall conclusion : formula,
+(*  Theorem soundness_of_propositional_logic :
       forall hypotheses : list formula,
+      forall conclusion : formula,
       infers hypotheses conclusion ->
       entails hypotheses conclusion.
     Proof.
     Qed.
+*)
 
   End Soundness.
 
   Section Completeness.
 
-    Theorem completeness_of_propositional_logic :
+(*  Theorem completeness_of_propositional_logic :
       forall consequence : formula,
       forall premises : list formula,
       entails premises consequence ->
       infers premises consequence.
     Proof.
     Qed.
+*)
 
   End Completeness.
 
