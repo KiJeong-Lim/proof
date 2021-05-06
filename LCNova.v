9a5f02459fe0dc93f8334162b1aa522d618eefce
KiJeong-Lim <gijungdduk@naver.com>
2021-05-06 22:37:26 +0900

    .

--- LCNova.v
+++ LCNova.v
@@ -225,8 +225,67 @@ Module AuxiliaPalatina.
       exists i.
       lia.
   Qed.
-    
+
+  Fixpoint FinSet_eqb {n1 : nat} {n2 : nat} (fs1 : FinSet n1) (fs2 : FinSet n2) : bool :=
+    match fs1 with
+    | FinSetZ _ =>
+      match fs2 with
+      | FinSetZ _ => true
+      | FinSetS _ fs2' => false 
+      end
+    | FinSetS _ fs1' =>
+      match fs2 with
+      | FinSetZ _ => false
+      | FinSetS _ fs2' => FinSet_eqb fs1' fs2'
+      end
+    end
+  .
+
   End forFinSet.
+
+  Section forZip.
+
+  Fixpoint zip {A : Type} {B : Type} (xs : list A) (ys : list B) : list (A * B) :=
+    match xs, ys with
+    | x :: xs', y :: ys' => (x, y) :: zip xs' ys'
+    | _, _ => []
+    end
+  .
+
+  Lemma zip_main_property {A : Type} {B : Type} :
+    forall xs : list A,
+    forall ys : list B,
+    length xs = length ys ->
+    let zs : list (A * B) := zip xs ys in
+    map fst zs = xs /\ map snd zs = ys.
+  Proof.
+    intros xs.
+    induction xs.
+    - intros ys.
+      destruct ys.
+      * intros.
+        simpl.
+        tauto.
+      * intros.
+        inversion H.
+    - intros ys.
+      destruct ys.
+      * intros.
+        inversion H.
+      * intros.
+        inversion H.
+        assert (map fst (zip xs ys) = xs /\ map snd (zip xs ys) = ys).
+          apply IHxs.
+          tauto.
+        destruct H0.
+        unfold zs.
+        simpl.
+        rewrite H0.
+        rewrite H2.
+        tauto.
+  Qed.
+
+  End forZip.
 
 End AuxiliaPalatina.
 
@@ -803,101 +862,378 @@ Module UntypedLambdaCalculus.
       else runSubst_Var sigma' x
     end
   .
-
   End Syntax.
 
-  Section AlphaEquiv.
+  Section DeBruijn.
 
-  Fixpoint getDeBruijnIndex (Gamma : list IVar) (x : IVar) : option (FinSet (length Gamma)) :=
-    match Gamma as ctx return option (FinSet (length ctx)) with
+  Fixpoint getDeBruijnIndex (Gamma : list IVar) (z : IVar) : option nat :=
+    match Gamma with
     | [] => None
-    | z :: Gamma' =>
+    | x :: Gamma' => 
       if IVar_eq_dec x z
-      then Some (FinSetZ (length Gamma'))
+      then Some 0
       else
-        match getDeBruijnIndex Gamma' x with
-        | None => None
-        | Some fs => Some (FinSetS _ fs)
-        end
+      match getDeBruijnIndex Gamma' z with
+      | None => None
+      | Some n => Some (S n)
+      end
     end
   .
 
-  Inductive DeBruijnTerm : nat -> Set :=
-  | DBTermBVar : forall n : nat, FinSet n -> DeBruijnTerm n
-  | DBTermFVar : forall n : nat, IVar -> DeBruijnTerm n
-  | DBTermIApp : forall n : nat, DeBruijnTerm n -> DeBruijnTerm n -> DeBruijnTerm n
-  | DBTermIAbs : forall n : nat, DeBruijnTerm (S n) -> DeBruijnTerm n
+  Lemma getDeBruijnIndex_property1 :
+    forall Gamma : list IVar,
+    forall z : IVar,
+    getDeBruijnIndex Gamma z = None <-> ~ In z Gamma.
+  Proof.
+    intros Gamma.
+    induction Gamma.
+    - intros.
+      simpl.
+      tauto.
+    - intros.
+      simpl.
+      destruct (IVar_eq_dec a z).
+      * constructor.
+        + intros.
+          inversion H.
+        + intros.
+          contradiction H.
+          tauto.
+      * assert (getDeBruijnIndex Gamma z = None <-> ~ In z Gamma).
+          apply IHGamma.
+        constructor.
+        + intros.
+          destruct (getDeBruijnIndex Gamma z).
+          { inversion H0.
+          }
+          { tauto.
+          }
+        + intros.
+          destruct (getDeBruijnIndex Gamma z).
+          { contradiction H0.
+            right.
+            assert (~ Some n0 = None).
+              intro.
+              inversion H1.
+            tauto.
+          }
+          { reflexivity.
+          }
+  Qed.
+
+  Lemma getDeBruijnIndex_property2 :
+    forall Gamma : list IVar,
+    forall z : IVar,
+    In z Gamma ->
+    exists idx : nat, getDeBruijnIndex Gamma z = Some idx.
+  Proof.
+    intros Gamma.
+    induction Gamma.
+    - intros.
+      inversion H.
+    - intros.
+      simpl in H.
+      destruct (IVar_eq_dec a z).
+      * exists 0.
+        simpl.
+        destruct (IVar_eq_dec a z).
+        + reflexivity.
+        + contradiction n.
+      * assert (In z Gamma).
+          tauto.
+        destruct (IHGamma z H0) as [idx].
+        exists (S idx).
+        simpl.
+        rewrite H1.
+        destruct (IVar_eq_dec a z).
+        + contradiction n.
+        + reflexivity.
+  Qed.
+
+  Lemma getDeBruijnIndex_property3 :
+    forall Gamma : list IVar,
+    forall z : IVar,
+    forall idx : nat,
+    getDeBruijnIndex Gamma z = Some idx -> 
+    exists Gamma1 : list IVar,
+    exists Gamma2 : list IVar,
+    ((Gamma = Gamma1 ++ [z] ++ Gamma2 /\ length Gamma1 = idx) /\ (forall x : IVar, In x Gamma1 -> x <> z)).
+  Proof.
+    intros Gamma.
+    induction Gamma.
+    - simpl.
+      intros.
+      inversion H.
+    - simpl.
+      intros.
+      destruct (IVar_eq_dec a z).
+      * inversion H.
+        subst.
+        exists [].
+        exists Gamma.
+        constructor.
+        + tauto.
+        + intros.
+          inversion H0.
+      * assert (
+          forall idx : IVar,
+          getDeBruijnIndex Gamma z = Some idx ->
+          exists Gamma1 : list IVar,
+          exists Gamma2 : list IVar,
+          (Gamma = Gamma1 ++ [z] ++ Gamma2 /\ length Gamma1 = idx) /\ (forall x : IVar, In x Gamma1 -> x <> z)
+        ).
+          apply IHGamma.
+        destruct (getDeBruijnIndex Gamma z).
+        + inversion H.
+          subst.
+          assert (
+            exists Gamma1 : list IVar,
+            exists Gamma2 : list IVar,
+            (Gamma = Gamma1 ++ [z] ++ Gamma2 /\ length Gamma1 = n0) /\ (forall x : IVar, In x Gamma1 -> x <> z)
+          ).
+            apply H0.
+            reflexivity.
+          destruct H1 as [Gamma1].
+          destruct H1 as [Gamma2].
+          destruct H1.
+          destruct H1.
+          exists (a :: Gamma1).
+          exists Gamma2.
+          constructor.
+          constructor.
+          rewrite H1.
+          simpl.
+          reflexivity.
+          simpl.
+          rewrite H3.
+          reflexivity.
+          intros.
+          simpl in H4.
+          destruct H4.
+          subst.
+          apply n.
+          apply H2.
+          apply H4.
+        + inversion H.
+  Qed.
+
+  Inductive DeBruijnTerm : Set :=
+  | BVar : nat -> DeBruijnTerm
+  | FVar : IVar -> DeBruijnTerm
+  | IApp : DeBruijnTerm -> DeBruijnTerm -> DeBruijnTerm
+  | IAbs : DeBruijnTerm -> DeBruijnTerm
   .
 
-  Fixpoint makeDeBruijnTerm_aux (Gamma : list IVar) (M : Term) : DeBruijnTerm (length Gamma) :=
+  Fixpoint makeDeBruijnTerm_aux (Gamma : list IVar) (M : Term) : DeBruijnTerm :=
     match M with
     | Var x =>
       match getDeBruijnIndex Gamma x with
-      | None => DBTermFVar _ x
-      | Some idx => DBTermBVar _ idx
+      | None => FVar x
+      | Some idx => BVar idx
       end
-    | App P1 P2 => DBTermIApp _ (makeDeBruijnTerm_aux Gamma P1) (makeDeBruijnTerm_aux Gamma P2)
-    | Lam y Q => DBTermIAbs _ (makeDeBruijnTerm_aux (y :: Gamma) Q)
+    | App P1 P2 => IApp (makeDeBruijnTerm_aux Gamma P1) (makeDeBruijnTerm_aux Gamma P2)
+    | Lam y Q => IAbs (makeDeBruijnTerm_aux (y :: Gamma) Q)
     end
   .
 
-  Definition makeDeBruijnTerm : Term -> DeBruijnTerm 0 :=
+  Inductive mapsToDeBruijn : list IVar -> Term -> DeBruijnTerm -> Prop :=
+  | mapsToDeBruijnBVar :
+    forall Gamma : list IVar,
+    forall x : IVar,
+    forall idx : nat,
+    getDeBruijnIndex Gamma x = Some idx ->
+    mapsToDeBruijn Gamma (Var x) (BVar idx)
+  | mapsToDeBruijnFVar :
+    forall Gamma : list IVar,
+    forall x : IVar,
+    getDeBruijnIndex Gamma x = None ->
+    mapsToDeBruijn Gamma (Var x) (FVar x)
+  | mapsToDeBruijnIApp :
+    forall Gamma : list IVar,
+    forall P1 : Term,
+    forall P2 : Term,
+    forall P1' : DeBruijnTerm,
+    forall P2' : DeBruijnTerm,
+    mapsToDeBruijn Gamma P1 P1' ->
+    mapsToDeBruijn Gamma P2 P2' ->
+    mapsToDeBruijn Gamma (App P1 P2) (IApp P1' P2')
+  | mapsToDeBruijnIAbs :
+    forall Gamma : list IVar,
+    forall y : IVar,
+    forall Q : Term,
+    forall Q' : DeBruijnTerm,
+    mapsToDeBruijn (y :: Gamma) Q Q' ->
+    mapsToDeBruijn Gamma (Lam y Q) (IAbs Q')
+  .
+
+  Lemma makeDeBruijnTerm_aux_main_property :
+    forall M : Term,
+    forall M' : DeBruijnTerm,
+    forall Gamma : list IVar,
+    M' = makeDeBruijnTerm_aux Gamma M <-> mapsToDeBruijn Gamma M M'.
+  Proof.
+    intros M.
+    induction M.
+    - intros.
+      simpl.
+      cut (
+        let idx' : option nat := getDeBruijnIndex Gamma x in
+        idx' = getDeBruijnIndex Gamma x ->
+        M' = (match idx' with | Some idx => BVar idx | None => FVar x end) <-> mapsToDeBruijn Gamma (Var x) M'
+      ).
+        intros.
+        apply H.
+        reflexivity.
+      intros.
+      destruct idx'.
+      * constructor.
+        + intros.
+          subst.
+          apply (mapsToDeBruijnBVar Gamma x n).
+          rewrite H.
+          reflexivity.
+        + intros.
+          inversion H0.
+          { subst.
+            rewrite H3 in H.
+            inversion H.
+            subst.
+            reflexivity.
+          }
+          { subst.
+            rewrite H3 in H.
+            inversion H.
+          }
+      * constructor.
+        + intros.
+          subst.
+          apply (mapsToDeBruijnFVar Gamma x).
+          rewrite H.
+          reflexivity.
+        + intros.
+          inversion H0.
+          { subst.
+            rewrite H3 in H.
+            inversion H.
+          }
+          { subst.
+            rewrite H3 in H.
+            inversion H.
+            subst.
+            reflexivity.
+          }
+    - intros.
+      constructor.
+      * intros.
+        subst.
+        simpl.
+        apply mapsToDeBruijnIApp.
+        apply IHM1.
+        reflexivity.
+        apply IHM2.
+        reflexivity.
+      * intros.
+        inversion H.
+        subst.
+        simpl.
+        assert (P1' = makeDeBruijnTerm_aux Gamma M1).
+          apply IHM1.
+          apply H3.
+        assert (P2' = makeDeBruijnTerm_aux Gamma M2).
+          apply IHM2.
+          apply H5.
+        rewrite H0.
+        rewrite H1.
+        reflexivity.
+    - intros.
+      constructor.
+      * intros.
+        subst.
+        simpl.
+        apply mapsToDeBruijnIAbs.
+        apply IHM.
+        reflexivity.
+      * intros.
+        inversion H.
+        subst.
+        simpl.
+        assert (Q' = makeDeBruijnTerm_aux (y :: Gamma) M).
+          apply IHM.
+          apply H4.
+        rewrite H0.
+        reflexivity.
+  Qed.
+
+  Definition makeDeBruijnTerm : Term -> DeBruijnTerm :=
     makeDeBruijnTerm_aux []
   .
 
-  Definition AlphaEquivByDeBruijn (M1 : Term) (M2 : Term) : Prop :=
-    makeDeBruijnTerm M1 = makeDeBruijnTerm M2
+  Fixpoint testCtx (Phi : list IVar -> Prop) (Gamma : list IVar) (N : Term) (M : Term) (X : IsSubtermOf N M) : Prop :=
+    match X with
+    | IsSubtermOfRefl N0 => Phi Gamma
+    | IsSubtermOfApp1 N0 P1 P2 X1 => testCtx Phi Gamma N0 P1 X1
+    | IsSubtermOfApp2 N0 P1 P2 X2 => testCtx Phi Gamma N0 P2 X2
+    | IsSubtermOfLam0 N0 y Q X0 => testCtx Phi (y :: Gamma) N0 Q X0
+    end
   .
 
-  Fixpoint AlphaEquivBySubst_aux (sigma : Subst) (M1 : Term) (M2 : Term) : bool :=
+  Lemma testCtx_main_property :
+    forall N : Term,
+    forall M : Term,
+    forall X : IsSubtermOf M N,
+    forall Gamma : list IVar,
+    mapsToDeBruijn Gamma N (makeDeBruijnTerm_aux Gamma N) ->
+    testCtx (fun ctx : list IVar => mapsToDeBruijn ctx M (makeDeBruijnTerm_aux ctx M)) Gamma M N X.
+  Proof.
+    intros N M X.
+    induction X.
+    - simpl.
+      intros.
+      tauto.
+    - simpl.
+      intros.
+      inversion H.
+      subst.
+      apply IHX.
+      apply H4.
+    - simpl.
+      intros.
+      inversion H.
+      subst.
+      apply IHX.
+      apply H6.
+    - simpl.
+      intros.
+      inversion H.
+      subst.
+      apply IHX.
+      apply H2.
+  Qed.
+
+  End DeBruijn.
+
+  Section AlphaEquiv.
+
+  Fixpoint checkAlphaEquivWithSubst (sigma : Subst) (M1 : Term) (M2 : Term) : bool :=
     match M1 with
-    | Var x =>
-      if Term_eq_dec (runSubst_Var sigma x) M2
+    | Var x1 =>
+      if Term_eq_dec (runSubst_Var sigma x1) M2
       then true
       else false
     | App P1_1 P2_1 =>
       match M2 with
-      | App P1_2 P2_2 => AlphaEquivBySubst_aux sigma P1_1 P1_2 && AlphaEquivBySubst_aux sigma P2_1 P2_2
+      | App P1_2 P2_2 => checkAlphaEquivWithSubst sigma P1_1 P1_2 && checkAlphaEquivWithSubst sigma P2_1 P2_2
       | _ => false
       end
     | Lam y1 Q1 =>
       match M2 with
-      | Lam y2 Q2 => AlphaEquivBySubst_aux ((y1, Var y2) :: sigma) Q1 Q2
+      | Lam y2 Q2 => checkAlphaEquivWithSubst ((y1, Var y2) :: sigma) Q1 Q2
       | _ => false
       end
     end
   .
 
-  Hypothesis AlphaEquivBySubst_aux_main_property :
-    forall M1 : Term,
-    forall M2 : Term,
-    forall dbctx : list (IVar * IVar),
-    forall H : length (map snd dbctx) = length (map fst dbctx),
-    AlphaEquivBySubst_aux (map (fun p : IVar * IVar => (fst p, Var (snd p))) dbctx) M1 M2 = true <-> makeDeBruijnTerm_aux (map fst dbctx) M1 = eq_rect (length (map snd dbctx)) DeBruijnTerm (makeDeBruijnTerm_aux (map snd dbctx) M2) (length (map fst dbctx)) H.
- 
-  Definition AlphaEquivBySubst (M1 : Term) (M2 : Term) : Prop :=
-    AlphaEquivBySubst_aux [] M1 M2 = true
-  .
-
-  Theorem AlphaEquivBySubst_main_property :
-    forall M1 : Term,
-    forall M2 : Term,
-    AlphaEquivBySubst M1 M2 <-> makeDeBruijnTerm M1 = makeDeBruijnTerm M2.
-  Proof.
-    intros.
-    unfold AlphaEquivBySubst.
-    assert (
-      let dbctx : list (IVar * IVar) := [] in
-      forall H : length (map snd dbctx) = length (map fst dbctx),
-      AlphaEquivBySubst_aux (map (fun p : IVar * IVar => (fst p, Var (snd p))) dbctx) M1 M2 = true <-> makeDeBruijnTerm_aux (map fst dbctx) M1 = eq_rect (length (map snd dbctx)) DeBruijnTerm (makeDeBruijnTerm_aux  (map snd dbctx) M2) (length (map fst dbctx)) H
-    ).
-      apply AlphaEquivBySubst_aux_main_property.
-    simpl in H.
-    assert (AlphaEquivBySubst_aux [] M1 M2 = true <-> makeDeBruijnTerm_aux [] M1 = eq_rect 0 DeBruijnTerm (makeDeBruijnTerm_aux [] M2) 0 eq_refl).
-      apply H.
-    simpl in H0.
-    apply H0.
-  Qed.
+  
 
   End AlphaEquiv.
 
